d08798b14f119e65ac5fd952e35b299f757d6288
Charles Santana <charles.santana@gmail.com>
2020-04-24 03:16:58 +0200

    Update at April 23rd

--- script.R
+++ script.R
@@ -2,17 +2,17 @@ library(tidyverse)
 library(reshape)
 library(tidyr)
 library(lubridate)
-anos <- c(2018,2019,2020)
-mortessp <- c(6769,6734,7755)
-mortesma <- c(507,402,905)
+anos <- c(2015,2016,2017,2018,2019,2020)
+mortesma <- c(148,166,410,507,402,905)
 
-df <- data.frame(anos = anos, `São Paulo` = mortessp, Manaus = mortesma)
-obitos_registros <- gather(df, cidade, mortes, `São.Paulo`:Manaus, factor_key=TRUE)
-
+df <- data.frame(anos = anos, Manaus = mortesma)
+obitos_registros <- gather(df, cidade, mortes, Manaus, factor_key=TRUE)
 casos = read.csv("caso.csv")
+simam = read.csv("SIMAM2018.csv")
+
 mean_deaths <- obitos_registros %>% 
    filter(cidade=="Manaus",
-          anos %in% c(2018,2019)) %>%
+          anos %in% c(2015,2016,2017,2018,2019)) %>%
    select(mortes)
 p1 <- obitos_registros %>% 
    filter(cidade=="Manaus") %>%
@@ -36,8 +36,9 @@ max_deaths <- casos %>%
    filter(city == "Manaus") %>%
    filter(is_last == "True") %>%
    select(deaths)
-march_deaths <- casos %>% filter(city == "Manaus",
-                                 date == "2020-03-31") %>%
+march_deaths <- casos %>% 
+   filter(city == "Manaus",
+          date == "2020-03-31") %>%
    select(deaths)
 min_date <- casos %>% 
    filter(city == "Manaus") %>%
@@ -58,3 +59,21 @@ png("mortes_confirmadas_covid19_manaus.png",width=3200,height=1800,res=300)
 print(p2)
 dev.off()
 
+italy_max_death100k = 10.5
+p3 <- casos %>% filter(city == "Manaus") %>% 
+   mutate(pop = confirmed*100000/confirmed_per_100k_inhabitants) %>% 
+   mutate(deaths_per_100k_inhabitants = deaths*100000/pop) %>%
+   mutate(date = ymd(date)) %>%
+   ggplot(aes(x = date, y = deaths_per_100k_inhabitants)) +
+   geom_line() + 
+   theme_bw()+
+   labs(title = "Taxa de letalidade por 100 Mil habitantes",
+        subtitle = "Manaus")+
+   geom_hline(yintercept=c(as.numeric(italy_max_death100k)), linetype="dashed", color = "red")+
+   scale_y_continuous(breaks = c(pretty(casos$deaths_per_100k_inhabitants), as.numeric(italy_max_death100k)), 
+                      labels = c(pretty(casos$deaths_per_100k_inhabitants), as.numeric(italy_max_death100k)))
+
+
+png("mortes_confirmadas_covid19_100k_hab_manaus.png",width=3200,height=1800,res=300)
+print(p3)
+dev.off()
