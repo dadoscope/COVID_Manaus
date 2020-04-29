752c9e01d62d9c0cdda1638593e86b7b7414694d
cndesantana <charles.santana@gmail.com>
2020-04-29 03:00:22 +0200

    Update

--- script.R
+++ script.R
@@ -2,8 +2,10 @@ library(tidyverse)
 library(reshape)
 library(tidyr)
 library(lubridate)
+setwd("/Users/isiscosta/RScript/COVID_Manaus")
 anos <- c(2015,2016,2017,2018,2019,2020)
 mortesma <- c(148,166,410,507,402,905)
+barcolor <- "#b5403e"
 
 df <- data.frame(anos = anos, Manaus = mortesma)
 obitos_registros <- gather(df, cidade, mortes, Manaus, factor_key=TRUE)
@@ -16,13 +18,13 @@ mean_deaths <- obitos_registros %>%
    select(mortes)
 p1 <- obitos_registros %>% 
    filter(cidade=="Manaus") %>%
-   ggplot(aes(x = anos, y = mortes, fill=cidade)) +
+   ggplot(aes(x = anos, y = mortes)) +
    geom_text(aes(x = anos, y = mortes, label = mortes, vjust = 0)) +
-   geom_bar(stat="identity") + 
+   geom_bar(stat="identity", fill = barcolor) + 
    labs(title = "Óbitos no Mês de Março", 
         subtitle = "Dados de registro civil")+
    theme_bw()+
-   geom_hline(yintercept=as.numeric(mean(mean_deaths$mortes)), linetype="dashed", color = "red")+
+   geom_hline(yintercept=as.numeric(mean(mean_deaths$mortes)), linetype="dashed", color = "blue")+
    scale_y_continuous(breaks = c(pretty(obitos_registros$mortes), as.numeric(mean(mean_deaths$mortes))), 
                       labels = c(pretty(obitos_registros$mortes), as.numeric(mean(mean_deaths$mortes))))
 
@@ -47,11 +49,13 @@ min_date <- casos %>%
 p2 <- casos %>% filter(city == "Manaus") %>%
    mutate(date = ymd(date)) %>%
    ggplot(aes(x = date, y = deaths)) +
-   geom_line() + 
+   geom_line(size=1.2) + 
    theme_bw()+
    labs(title = "Acumulado de óbitos por covid19 confirmados",
-        subtitle = "Manaus")+
-   geom_hline(yintercept=c(as.numeric(march_deaths$deaths),as.numeric(max_deaths$deaths)), linetype="dashed", color = "red")+
+        subtitle = "Manaus",
+        x = "Data",
+        y = "# de Mortes")+
+   geom_hline(yintercept=c(as.numeric(max_deaths$deaths)), linetype="dashed", color = "blue")+
    scale_y_continuous(breaks = c(pretty(casos$deaths), as.numeric(max_deaths)), 
                       labels = c(pretty(casos$deaths), as.numeric(max_deaths)))
    
@@ -60,20 +64,53 @@ print(p2)
 dev.off()
 
 italy_max_death100k = 10.5
+
 p3 <- casos %>% filter(city == "Manaus") %>% 
-   mutate(pop = confirmed*100000/confirmed_per_100k_inhabitants) %>% 
+   mutate(pop = 2182763) %>% 
    mutate(deaths_per_100k_inhabitants = deaths*100000/pop) %>%
    mutate(date = ymd(date)) %>%
    ggplot(aes(x = date, y = deaths_per_100k_inhabitants)) +
-   geom_line() + 
+   geom_line(size=1.2) + 
    theme_bw()+
    labs(title = "Taxa de letalidade por 100 Mil habitantes",
-        subtitle = "Manaus")+
-   geom_hline(yintercept=c(as.numeric(italy_max_death100k)), linetype="dashed", color = "red")+
-   scale_y_continuous(breaks = c(pretty(casos$deaths_per_100k_inhabitants), as.numeric(italy_max_death100k)), 
-                      labels = c(pretty(casos$deaths_per_100k_inhabitants), as.numeric(italy_max_death100k)))
+        subtitle = "Manaus",
+        x = "Data",
+        y = "Mortes/100k habitantes")+
+   geom_hline(yintercept=c(11.27), linetype="dashed", color = "red")+
+   geom_hline(yintercept=c(as.numeric(italy_max_death100k)), linetype="dashed", color = "blue")+
+   scale_y_continuous(breaks = c(pretty(casos$deaths_per_100k_inhabitants), as.numeric(italy_max_death100k), 11.27), 
+                      labels = c(pretty(casos$deaths_per_100k_inhabitants), as.numeric(italy_max_death100k), 11.27))
 
 
 png("mortes_confirmadas_covid19_100k_hab_manaus.png",width=3200,height=1800,res=300)
 print(p3)
 dev.off()
+
+### Abril 2020
+
+anos <- c(2015,2016,2017,2018,2019,2020)
+mortesma <- c(104 ,417,157,266,591,1676 )
+barcolor <- "#b5403e"
+df <- data.frame(anos = anos, Manaus = mortesma)
+obitos_registros <- gather(df, cidade, mortes, Manaus, factor_key=TRUE)
+
+mean_deaths <- obitos_registros %>% 
+  filter(cidade=="Manaus",
+         anos %in% c(2015,2016,2017,2018,2019)) %>%
+  select(mortes)
+p1 <- obitos_registros %>% 
+  filter(cidade=="Manaus") %>%
+  ggplot(aes(x = anos, y = mortes)) +
+  geom_text(aes(x = anos, y = mortes, label = mortes, vjust = 0)) +
+  geom_bar(stat="identity", fill = barcolor) + 
+  labs(title = "Óbitos no Mês de Abril", 
+       subtitle = "Dados de registro civil")+
+  theme_bw()+
+  geom_hline(yintercept=as.numeric(mean(mean_deaths$mortes)), linetype="dashed", color = "blue")+
+  scale_y_continuous(breaks = c(pretty(obitos_registros$mortes), as.numeric(mean(mean_deaths$mortes))), 
+                     labels = c(pretty(obitos_registros$mortes), as.numeric(mean(mean_deaths$mortes))))
+
+png("mortes_em_manaus_abril.png",width=3200,height=1800,res=300)
+print(p1)
+dev.off()
+
